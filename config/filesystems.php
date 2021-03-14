b4b8002e8fcb4295233d92c1a4b9fef3ff763d90
chenneyccc <chenneycc@outlook.com>
2021-03-14 11:19:51 +0100

    use errorlog

--- config/filesystems.php
+++ config/filesystems.php
@@ -52,6 +52,11 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'images' => [
+            'driver' => 'local',
+            'root'   => public_path() . '/images',
+        ],
+
     ],
 
     /*
